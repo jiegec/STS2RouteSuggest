7b677ec05da58538fd251061d8acc2851d5aa9b5
Jiajie Chen <c@jia.je>
2026-03-13 17:24:15 +0800

    Add v0.98.3 support

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ case "$OS" in
         mkdir -p dist
         cp ./.godot/mono/temp/bin/Debug/RouteSuggest.dll dist/
         ../Godot_v4.5.1-stable_mono_linux_x86_64/Godot_v4.5.1-stable_mono_linux.x86_64 --export-pack "Windows Desktop" dist/RouteSuggest.pck --headless
-        cp mod_manifest.json dist/RouteSuggest.json
+        cp RouteSuggest.json dist/RouteSuggest.json
         ;;
     Darwin*)
         cp ~/Library/Application\ Support/steam/steamapps/common/Slay\ the\ Spire\ 2/SlayTheSpire2.app/Contents/Resources/data_sts2_macos_arm64/sts2.dll .
@@ -20,7 +20,7 @@ case "$OS" in
         mkdir -p dist
         cp ./.godot/mono/temp/bin/Debug/RouteSuggest.dll dist/
         /Applications/Godot_mono.app/Contents/MacOS/Godot --export-pack "Windows Desktop" dist/RouteSuggest.pck --headless
-        cp mod_manifest.json dist/RouteSuggest.json
+        cp RouteSuggest.json dist/RouteSuggest.json
         ;;
     *)
         echo "Unknown operating system: $OS"
