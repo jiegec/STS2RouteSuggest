4324bf2a4653db2a06ff6fcab64e86565bced2ad
Jiajie Chen <c@jia.je>
2026-06-19 17:09:45 +0800

    Copy from README to workshop.json

--- publish.sh
+++ publish.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+python3 -c "
+import json
+with open('README.md') as f:
+    desc = f.read()
+    desc = desc[:desc.find('## Building from Source')]
+with open('SteamWorkshop/workshop.json') as f:
+    ws = json.load(f)
+ws['description'] = desc
+with open('SteamWorkshop/workshop.json', 'w') as f:
+    json.dump(ws, f, indent=2)
+"
 cp dist/* SteamWorkshop/content
 cp RouteSuggest/mod_image.png SteamWorkshop/image.png
 ./ModUploader-linux-x64/ModUploader upload -w $PWD/SteamWorkshop
