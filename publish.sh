ed1b5cff1a65f59c6f9ff40282548b2a306329b3
Jiajie Chen <c@jia.je>
2026-06-20 09:29:49 +0800

    Add chinese to workship info

--- publish.sh
+++ publish.sh
@@ -5,10 +5,14 @@ with open('README.md') as f:
     desc = f.read()
     changelog = desc[desc.find('## Changelog'):]
     desc = desc[:desc.find('## Building from Source')]
+with open('README_zh.md') as f:
+    desc_zh = f.read()
+    changelog_zh = desc_zh[desc_zh.find('## 更新日志'):]
+    desc_zh = desc_zh[:desc_zh.find('## 手动安装方法')]
 with open('SteamWorkshop/workshop.json') as f:
     ws = json.load(f)
-ws['description'] = desc
-ws['changeNote'] = changelog
+ws['description'] = desc + desc_zh
+ws['changeNote'] = changelog + changelog_zh
 with open('SteamWorkshop/workshop.json', 'w') as f:
     json.dump(ws, f, indent=2)
 "
